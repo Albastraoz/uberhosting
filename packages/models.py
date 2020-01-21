903c3ff1a71b984924d00a1afe47f2403682a037
Robin Kaal <rkaal7@gmail.com>
2020-01-21 16:55:32 +0100

    Version 1.11 - Added highlighted package function.

--- packages/models.py
+++ packages/models.py
@@ -3,14 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Package(models.Model):
-    name = models.CharField(max_length=50, default='')
+    name = models.CharField(max_length=50, default='Package name')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     domains = models.IntegerField(default=1)
-    mail_space = models.IntegerField()
-    mail_address = models.IntegerField()
-    hosting_space = models.IntegerField()
-    hosting_databases = models.IntegerField()
+    mail_space = models.IntegerField(default=0)
+    mail_address = models.IntegerField(default=0)
+    hosting_space = models.IntegerField(default=0)
+    hosting_databases = models.IntegerField(default=0)
     apps = models.BooleanField(default=False)
+    highlight = models.BooleanField(default=False)
     sftp = models.IntegerField(default=0)
 
     def __str__(self):
