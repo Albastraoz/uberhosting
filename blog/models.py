8ce1720138fa30a4c89383f90092fa152d4f5082
Robin Kaal <rkaal7@gmail.com>
2020-01-26 16:18:54 +0100

    Version 1.25 - Added superuser only access to posting and editing posts on blog.

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,9 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    author = models.CharField(max_length=200, blank=False)
+    title = models.CharField(max_length=200, blank=False)
+    content = models.TextField(blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     views = models.IntegerField(default=0)
     
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
@@ -22,13 +23,15 @@ def post_detail(request, pk):
 @login_required()
 def create_or_edit_post(request, pk=None):
     # Create a post or edit an existing post.
-
-    post = get_object_or_404(Post, pk=pk) if pk else None
-    if request.method == "POST":
-        form = BlogPostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save()
-            return redirect(post_detail, post.pk)
+    if request.user.is_superuser:
+        post = get_object_or_404(Post, pk=pk) if pk else None
+        if request.method == "POST":
+            form = BlogPostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save()
+                return redirect(post_detail, post.pk)
+        else:
+            form = BlogPostForm(instance=post)
+        return render(request, 'blogpostform.html', {'form': form})
     else:
-        form = BlogPostForm(instance=post)
-    return render(request, 'blogpostform.html', {'form': form})
+        return redirect(get_posts)
