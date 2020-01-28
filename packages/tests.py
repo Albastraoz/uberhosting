3a60b27f1e47300ae51ea5eca394b43b7db5eff5
Robin Kaal <rkaal7@gmail.com>
2020-01-28 14:05:30 +0100

    Version 1.44 - Added testcase to package app and put secret key for travis in travis environ keys.

--- packages/tests.py
+++ packages/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Package
 
 # Create your tests here.
+class PackageTests(TestCase):
+    """
+    Here we'll define the tests that we'll run against our
+    Package model
+    """
+
+    def test_str(self):
+        test_name = Package(name='A product')
+        self.assertEqual(str(test_name), 'A product')
